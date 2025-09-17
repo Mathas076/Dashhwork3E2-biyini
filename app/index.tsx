bc6d731286b6f556bc9d4eddff3243fb20c38f8d
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-09-17 16:45:40 +0000

    ✨feat(components): -Crear la carpeta components -Crear el componente profile-details.tsx -Implementar profile-details a index.txs

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
 import "../global.css";
+import ProfileDeatils from "@/components/profile-details";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex justify-center items-center bg-white w-screen h-screen">
+      <Text className=" text-3xl font-bold text-orange-400">Hola.</Text>
+      <ProfileDeatils></ProfileDeatils>
     </View>
   );
 }
