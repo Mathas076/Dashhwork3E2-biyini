49aa8c35e646f3d27b08f837851c6d5902cbae70
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-09-28 23:09:39 +0000

    💄ui(index): Imprementacion de MoreDetails -Cambio de imagen en profile-details -Iconos en MoreDetails y agregar contactos

--- components/MoreDetails.tsx
+++ components/MoreDetails.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import IconButton from './ui/iconButton'
+import CustomText from './ui/CustomText'
 
 const MoreDetails = () => {
   return (
     <View className='h-1/2 w-screen bg-zinc-900 rounded-t-3xl p-6 flex flex-col justify-start gap-4'>
-      <Text className="text-xl font-bold text-gray-300">Skill</Text>
+      <CustomText variant='subtitle' value='Skill'/> 
       <View className="flex-row justify-center items-center gap-4">
         <IconButton icon="airplane" pulsar={() => console.log("Instagram")} color="black"></IconButton>
         <IconButton icon="accessibility" pulsar={() => console.log("Whatsapp")} color="orange"></IconButton>
